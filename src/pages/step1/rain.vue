1cfb9150d5146edf12862dd93d65da0b284bf1fd
cchao <2020807070@qq.com>
2020-06-15 00:11:52 +0800

    add some file

--- src/pages/step1/rain.vue
+++ src/pages/step1/rain.vue
@@ -4,40 +4,23 @@
       @touchstart="touchstart"
       @touchmove="touchmove"
       @touchend="touchend">
-      <div class="content">风</div>
+      <div class="content c1">风</div>
       <div class="content">还有雨……</div>
       <div class="move-wrapepr" @click="next">
-        <div class="move-container"></div>
+        <div class="move-container move-container__rain" :style="`background-position: center ${rainMovePostion}px;`"></div>
       </div>
       <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
       <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-      <div class="content">从<span>大革命</span>失败，共产党人起义反对蒋介石，到现在已经八年了。</div>
-      <div class="content">这八年里，共产党在全国农村已经建立起十几块革命根据地。其中最大的一块就是这里——</div>
-
+      <div class="move-wrapepr" @click="next">
+        <div class="move-container move-container__map" :style="`background-position: ${mapMovePostionX}px ${mapMovePostionY}px;`"
+          @touchstart="maptouchstart"
+          @touchmove="maptouchmove"
+          @touchend="maptouchend"></div>
+      </div>
+      <div class="title">中央苏区</div>
+      <div class="content">
+        我是中央苏区中一名普通的红军战士，同大部分战士一样，我们以为自己会一直保卫着这里。不过，后来的事情出乎我们的意料——
+      </div>
     </div>
   </div>
 </template>
@@ -46,6 +29,12 @@
 export default {
   data () {
     return {
+      baseHeight: 80,
+      rainMoveTop: 0,
+      rainMovePostion: 0,
+      mapMoveTop: 0,
+      mapMovePostionX: 0, // -80
+      mapMovePostionY: 0,
       start: {
         clientX: 0,
         clientY: 0
@@ -57,6 +46,10 @@ export default {
       scrollTop: 0,
       lastEnd: 0,
     }
+  },
+  mounted () {
+    this.rainMoveTop = document.querySelectorAll('.move-container')[0].offsetTop;
+    this.mapMoveTop = document.querySelectorAll('.move-container')[1].offsetTop;
   },
   methods: {
     touchstart (e) {
@@ -70,14 +63,34 @@ export default {
       const { clientY } = e.touches[0];
       this.scrollTop = clientY - this.start.clientY + this.lastEnd;
       if (this.scrollTop > 10)  this.scrollTop = 8;
-
+      this.checkMoveOver();
     },
     touchend () { 
       this.lastEnd = this.scrollTop;
     },
     next () {
       this.$router.replace('/home');
-    }
+    },
+    checkMoveOver () {
+      if(Math.abs(this.scrollTop) >= this.rainMoveTop + this.baseHeight) {
+        this.rainMovePostion = Math.abs(this.scrollTop) - this.rainMoveTop - this.baseHeight;
+      } else this.rainMovePostion = 0;
+      if(Math.abs(this.scrollTop) >= this.mapMoveTop + this.baseHeight) {
+        this.mapMovePostionY = Math.abs(this.scrollTop) - this.mapMoveTop - this.baseHeight;
+      } else this.mapMovePostionY = 0;
+    },
+    maptouchstart (e) {
+      const { clientX } = e.touches[0];
+      this.map = { clientX };
+    },
+    maptouchmove (e) {
+      e.stopPropagation();
+      console.log(this.map.clientX - e.touches[0].clientX);
+      this.mapMovePostionX = -(this.map.clientX - e.touches[0].clientX);
+    },
+    maptouchend (e) {
+      console.log(e);
+    },
   }
 }
 </script>
@@ -106,52 +119,44 @@ export default {
   }
 }
 
-.move-wrapepr {
-    height: 900px;
+.c1 {
+  margin-top: px2rem(10, 20);
 }
 
-.content {
-  margin-left: 50%;
-  transform: translateX(-50%);
-  width: px2rem(295, 20);
-  font-size: px2rem(18, 20);
-  font-family: 'fzFont';
-  line-height: px2rem(32, 20);
-  color: #222222;
-  span {
-    text-decoration: underline;
-    color: #e95430;
-  }
+.title {
+  font-family: "fzFont";
+  text-align: center;
+  margin: px2rem(11) 0;
+  font-size: px2rem(22, 20);
 }
-
 .move-wrapepr {
-  height: 200px;
   overflow: hidden;
+  height: px2rem(320 , 20);
+  margin: px2rem(30, 20);
 }
 
 
 .move-container {
-  animation-name: zoom;
-  animation-duration: 3.5;
-  animation-iteration-count: 1;
+  animation: zoom 10s infinite;
   width: 100%;
   height: 100%;
   background-size: cover;
-  background-position: 50% 50%;
-  background-image: url(./img/rain.jpg);
-  transform: scale(1);
+  background-repeat: no-repeat;
+  zoom: 1;
+  &__rain {
+    background-image: url(./img/rain.jpg);
+  }
+  &__map {
+    background-image: url(./img/map1.jpg);
+  }
 }
 
 @keyframes zoom {
   0% {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(1.5);
+    zoom: 1;
   }
   100% {
-    transform: scale(1);;
+    zoom: 1.8;
   }
 }
 
